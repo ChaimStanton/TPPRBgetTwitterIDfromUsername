8029662bd29dd683e8f7e814611aa36a2a1608fe
Chaim Stanton <38401049+ChaimStanton@users.noreply.github.com>
2020-07-09 03:17:07 +0100

    prints off the name

--- getTwitterIDfromUsername.py
+++ getTwitterIDfromUsername.py
@@ -4,10 +4,16 @@ from keysAuthentication import *
 
 import tweepy
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+# auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth)
+# api = tweepy.API(auth)
 
 # api.get_user(put id here )
 # http://docs.tweepy.org/en/v3.5.0/api.html?highlight=get_user#user-methods
+
+with open("usernames.txt", "r") as txtFile:
+    # listOfNames = txtFile.readlin()
+    for name in txtFile:
+        # print(name.rstrip("\n"))
+        print(name[:-2])
