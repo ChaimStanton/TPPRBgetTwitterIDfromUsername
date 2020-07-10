d2c62e02a3e74119bfffb45dcbd61153a395dfeb
Chaim Stanton <38401049+ChaimStanton@users.noreply.github.com>
2020-07-10 02:00:41 +0100

    rearranged files

--- getTwitterId/getTwitterIDfromUsername.py
+++ getTwitterId/getTwitterIDfromUsername.py
@@ -9,15 +9,11 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-# 
-# http://docs.tweepy.org/en/v3.5.0/api.html?highlight=get_user#user-methods
-
-
 
 txtFile = open("usernames.txt", "r")
 
 for name in txtFile:
     print(name.rstrip("\n"))
     # print(name[:-2])
-    user = api.get_user(name)
+    # user = api.get_user(name)
     print(user.id_str)
